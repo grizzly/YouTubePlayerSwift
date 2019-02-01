bcfce492e38abaa229818faac6ca17f0f8620c34
Stefan Mayr <st.mayr@grizzly.cc>
2019-02-01 14:33:18 +0100

    things

--- YouTubePlayerSwift.podspec
+++ YouTubePlayerSwift.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YouTubePlayerSwift'
-  s.version          = '0.1.3'
+  s.version          = '0.1.4'
   s.summary          = 'Easy to use library to play YouTubeVideos in fullscreen mode (Swift 4.2).'
 
 # This description is used to generate tags and improve search results.
--- YouTubePlayerSwift/Classes/YouTubePlayerSwift.swift
+++ YouTubePlayerSwift/Classes/YouTubePlayerSwift.swift
@@ -2,9 +2,10 @@
 import UIKit;
 import WebKit;
 
-open class YouTubePlayerView: UIView, WKScriptMessageHandler {
+open class YouTubePlayerView: UIView, WKScriptMessageHandler, WKNavigationDelegate {
     
-    fileprivate var webView: WKWebView?
+    fileprivate var webView: WKWebView!
+    fileprivate var spinner : UIActivityIndicatorView?;
     
     // MARK: Various methods for initialization
     
@@ -20,51 +21,53 @@ open class YouTubePlayerView: UIView, WKScriptMessageHandler {
     
     override open func layoutSubviews() {
         super.layoutSubviews()
+        
         // Remove web view in case it's within view hierarchy, reset frame, add as subview
-        if let webView = self.webView {
-            webView.removeFromSuperview()
-            webView.frame = bounds
-            addSubview(webView)
-        }
+        webView.removeFromSuperview()
+        webView.frame = bounds
+        addSubview(webView)
     }
     
     // MARK: Web view initialization
     
     fileprivate func buildWebView() {
-        if self.webView == nil {
-            let preferences = WKPreferences()
-            preferences.javaScriptEnabled = true
-            let config = WKWebViewConfiguration()
-            config.preferences = preferences
-            config.mediaTypesRequiringUserActionForPlayback = [];
-            let userContent: WKUserContentController = WKUserContentController()
-            userContent.add(
-                self,
-                name: "callbackHandler"
-            )
-            config.userContentController = userContent
-            config.allowsInlineMediaPlayback = true
-            
-            let webView = WKWebView(frame: self.bounds, configuration: config)
-            let userAgent = UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent") ?? "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79"
-            webView.customUserAgent = userAgent + " Safari";
-            webView.isOpaque = false
-            webView.backgroundColor = UIColor.clear
-            webView.scrollView.isScrollEnabled = false
-            self.webView = webView;
-        }
+        
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
+        let config = WKWebViewConfiguration()
+        config.preferences = preferences
+        config.mediaTypesRequiringUserActionForPlayback = [];
+        let userContent: WKUserContentController = WKUserContentController()
+        userContent.add(
+            self,
+            name: "callbackHandler"
+        )
+        config.userContentController = userContent
+        config.allowsInlineMediaPlayback = true
+        
+        let webView = WKWebView(frame: self.bounds, configuration: config)
+        let userAgent = UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent") ?? "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79"
+        webView.customUserAgent = userAgent + " Safari";
+        webView.isOpaque = false
+        webView.backgroundColor = UIColor.clear
+        webView.scrollView.isScrollEnabled = false
+        webView.navigationDelegate = self;
+        self.webView = webView;
+        
+        
     }
     
     // MARK: Player setup
     
     public func play(videoID:String) {
-        if let webView = self.webView {
-            webView.load(URLRequest(url: URL(string: "https://shareme.pro/youtube/" + videoID)!));
+        DispatchQueue.main.async {
+            self.addSpinner();
+            self.webView.load(URLRequest(url: URL(string: "https://shareme.pro/youtube/" + videoID)!));
         }
     }
     
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        
+        print(message);
     }
     
     public static func videoIDFromYouTubeURL(_ videoURL: URL) -> String? {
@@ -75,7 +78,39 @@ open class YouTubePlayerView: UIView, WKScriptMessageHandler {
         }
         return videoURL.queryStringComponents()["v"] as? String
     }
-
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("YouTubePlayer> finished navigation");
+        self.removeSpinner();
+    }
+    
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("YouTubePlayer> didStartProvisionalNavigation");
+    }
+    
+    public func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("YouTubePlayer> didCommit");
+    }
+    
+    fileprivate func addSpinner() {
+        DispatchQueue.main.async {
+            if (self.spinner == nil) {
+                self.spinner = UIActivityIndicatorView(style: .gray);
+                self.spinner?.center = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2);
+            }
+            self.insertSubview(self.spinner!, aboveSubview: self.webView);
+            self.spinner?.startAnimating();
+        }
+    }
+    
+    fileprivate func removeSpinner() {
+        DispatchQueue.main.async {
+            self.spinner?.stopAnimating();
+            self.spinner?.removeFromSuperview();
+            self.spinner = nil;
+        }
+    }
+    
 }
 
 private extension URL {
